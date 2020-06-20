ccf278baa67973744a0016c918380e752cd54bbb
kuznet.i.sova <kis7425910@gmail.com>
2020-06-21 01:24:48 +0300

    added logic and images

---
+++ FirstParsingJsonApp/Models/Character.swift
@@ -0,0 +1,34 @@
+//
+//  Character.swift
+//  FirstParsingJsonApp
+//
+//  Created by Ирина Кузнецова on 20.06.2020.
+//  Copyright © 2020 Irina Kuznetsova. All rights reserved.
+//
+
+struct Character {
+    let nameCharacter: String
+    let classCharacter: String
+    let descriptionCharacter: String
+    let imageUrl: String
+    
+}
+
+extension Character {
+    static func getCharacter() -> [Character] {
+        var charactersList = [Character]()
+
+        for index in 0 ..< Characters().nameCharacters.count {
+            charactersList.append (
+                Character (
+                    nameCharacter: Characters().nameCharacters[index],
+                    classCharacter: Characters().classCharacters[index],
+                    descriptionCharacter: Characters().descriptionCharacters[index],
+                    imageUrl: Characters().imagesUrl[index]
+                )
+            )
+        }
+
+        return charactersList
+    }
+}
---
+++ FirstParsingJsonApp/Models/Characters.swift
@@ -0,0 +1,34 @@
+//
+//  Characters.swift
+//  FirstParsingJsonApp
+//
+//  Created by Ирина Кузнецова on 21.06.2020.
+//  Copyright © 2020 Irina Kuznetsova. All rights reserved.
+//
+
+struct Characters {
+    let nameCharacters = [
+        "Amara",
+        "FL4K",
+        "Moze",
+        "Zane"
+    ]
+    let classCharacters = [
+        "Siren",
+        "Beastmaster",
+        "Gunner",
+        "Operative"
+    ]
+    let descriptionCharacters = [
+        "Gifted with powerful Siren abilities, Amara is a renowned champion of the people and bonafide badass. Using her action skills she can Phaseslam the ground to damage nearby enemies and knock them into the air, Phasegrasp enemies in place with a giant ethereal fist, or even Phasecast an astral projection of herself that deals damage to everything in its path.",
+        "FL4K is a wandering robot who always brings one of their three loyal pets into battle to buff stats and attack enemies. Their action skills let them send forth dive-bombing Rakk, teleport Gamma Burst-irradiated pets through rifts, and even Fade Away to temporarily become invisible while moving faster and regenerating health.",
+        "Moze is a battle-hardened former Vladof army soldier who specializes in mechanized combat. She uses an action skill to digistruct and climb into her bipedal tank, Iron Bear, which has hardpoints suitable for mixing, matching, and mounting high-velocity Railguns, rapid-firing Miniguns, semi-automatic Grenade Launchers, flamethrowers, pneumatic fists, and homing rocket launchers.",
+        "Born into Pandora's notorious Flynt family, Zane is a semi-retired corporate hitman who always has a gadget up his sleeve. His action skills include a SNTNL drone that flys around and attacks enemies with a machine gun, a deployable Barrier shield, and a Digi-Clone that simultaneously distracts enemies and dishes out damage and which Zane can teleport to switch places with."
+    ]
+    let imagesUrl = [
+        "https://borderlands.com/images/characters/amara.png?6459e668162f4327b6ad38a9ae2a942f",
+        "https://borderlands.com/images/characters/fl4k.png?4efad7242bb1f1cce6d89f3ef7082267",
+        "https://borderlands.com/images/characters/moze.png?64fbc8fdb129e0cd45b148c11bb2241d",
+        "https://borderlands.com/images/characters/zane.png?ea582496c9c7fa1ae4cdc8211497934b"
+    ]
+}
