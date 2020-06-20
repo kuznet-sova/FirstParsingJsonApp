8299dd27790d4ef0e3e957cf50d69f6d3f7d2a60
kuznet.i.sova <kis7425910@gmail.com>
2020-06-20 21:56:21 +0300

    created structure of application

---
+++ FirstParsingJsonApp/CharacterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CharacterViewController.swift
+//  FirstParsingJsonApp
+//
+//  Created by Ирина Кузнецова on 20.06.2020.
+//  Copyright © 2020 Irina Kuznetsova. All rights reserved.
+//
+
+import UIKit
+
+class CharacterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
